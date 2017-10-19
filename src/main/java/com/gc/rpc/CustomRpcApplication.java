50fe95903124b7f8921227720a15229052cc20b2
guochang <guochang@sigo.cn>
2017-10-19 19:47:54 +0800

    测试提交 郭昶

---
+++ src/main/java/com/gc/rpc/CustomRpcApplication.java
@@ -0,0 +1,12 @@
+package com.gc.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomRpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomRpcApplication.class, args);
+	}
+}
